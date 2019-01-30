977c831ad5989e16daa47a2b3ff1abfe2da8ae5a
tri.ngo <ngominhtrint@gmail.com>
2019-01-30 22:12:55 +0800

    Setup project.

---
+++ BibleQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BibleQuiz
+//
+//  Created by TriNgo on 1/30/19.
+//  Copyright © 2019 RoverDream. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
